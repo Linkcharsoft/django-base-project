d488a65b90bcbf51cb3741f0a86c9a345a9c9134
Luca Citta Giordano <lucacitta@gmail.com>
2024-02-26 16:15:18 -0300

    [ ADD ] Collectstatic al entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 
 python manage.py migrate
 
+python manage.py collectstatic --noinput
+
 gunicorn -w 3 -b :8000 django_base.wsgi:application
 # daphne -b 0.0.0.0 -p 8000 django_base.asgi:application
