3fefe0a385530ffbdbf1c0fc2d48b0d8dfbdd0e4
Luca Citta Giordano <lucacitta@gmail.com>
2025-07-25 16:23:25 -0300

    [ REMOVE ] From requirements.in not specific libs and comment async ones by default

--- django_base/__init__.py
+++ django_base/__init__.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import, unicode_literals
 
 # Esto asegurará que la aplicación siempre se importe cuando
 # Django inicie para que se puedan usar las tareas compartidas.
-from django_base.celery import app as celery_app
+# from django_base.celery import app as celery_app
 
-__all__ = ("celery_app",)
+# __all__ = ("celery_app",)
